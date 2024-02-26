5a44a9a362e1932c931327d263d109939807fff9
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-02-26 14:11:30 -0500

    Added data to search to help label the results

--- src/schema/base/resolvers/SearchResult.ts
+++ src/schema/base/resolvers/SearchResult.ts
@@ -1,12 +1,12 @@
 import mappedSlugs from '../../../slugMapper';
 import type   { SearchResultResolvers } from './../../types.generated';
-    export const SearchResult: SearchResultResolvers = {
-      href: ({slug}) => {
-        const found = mappedSlugs.find(element => element.slug === slug);
-        console.info("slug " + slug + " found: ", found)
-        if(found)
-          return found.url
-        else
-          return
-      }
-  };
+export const SearchResult: SearchResultResolvers = {
+  href: ({slug}) => {
+    const found = mappedSlugs.find(element => element.slug === slug);
+    console.info("slug " + slug + " found: ", found)
+    if(found)
+      return found.url
+    else
+      return
+  }
+};
