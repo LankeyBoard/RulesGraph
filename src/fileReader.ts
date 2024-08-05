2bcb55c05060e89782520007d5fde3026836b6ce
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2024-08-04 22:26:07 -0400

    saves title and slug proplerly

--- src/fileReader.ts
+++ src/fileReader.ts
@@ -1,16 +1,43 @@
 import fs from "node:fs";
+import { GenericRule } from "./schema/types.generated";
 
-const readFile = (filename: string) => {
-  fs.readFile(filename, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
+const readFile = (filename: string): string => {
+  try {
+    const data = fs.readFileSync(filename, "utf8");
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+  return "";
+};
+
+const ruleArrayToRule = (rulesArray: string[], level: number) => {
+  if (level > 3) return;
+  console.log("incoming: ", rulesArray);
+  const rules: GenericRule[] = [];
+  rulesArray.forEach((rule) => {
+    const splitRule = rule.split(/\n/);
+    const newRule = {
+      title: splitRule[0],
+      slug: splitRule[1].slice(6),
+      subRules: ruleArrayToRule(
+        rule.split("[^#]" + "#".repeat(level) + " "),
+        level + 1
+      ),
+    };
+    rules.push(newRule);
   });
+  return rules;
+};
+
+const rulesStringToObj = (fileContents: string): GenericRule[] | undefined => {
+  console.log(fileContents);
+  return ruleArrayToRule(fileContents.split(/[^#]# /), 0);
 };
 
 function main() {
-  readFile("generalRules.md");
+  const fileContentsStr = readFile("generalRules.md");
+  const rules = rulesStringToObj(fileContentsStr);
+  console.log("rules data", rules);
 }
 main();
