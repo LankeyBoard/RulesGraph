d17e7711e792c2b38ed8e010a688515a9f2b494f
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2025-03-27 00:40:51 -0400

    implemented delete character mutation

--- src/schema/base/resolvers/Mutation/deleteCharacter.ts
+++ src/schema/base/resolvers/Mutation/deleteCharacter.ts
@@ -1,5 +1,17 @@
 import type { MutationResolvers } from "./../../../types.generated";
-export const deleteCharacter: NonNullable<MutationResolvers['deleteCharacter']> = async (_parent, _arg, _ctx) => {
-  /* Implement Mutation.deleteCharacter resolver logic here */
-  return false;
+export const deleteCharacter: NonNullable<
+  MutationResolvers["deleteCharacter"]
+> = async (_parent, _arg, _ctx) => {
+  if (!_ctx.currentUser) {
+    throw new Error("You must be logged in to create a character");
+  }
+  try {
+    await _ctx.prisma.character.delete({
+      where: { id: Number(_arg.id), createdBy: { id: _ctx.currentUser.id } },
+    });
+    return true;
+  } catch (error) {
+    console.error("Error deleting character", error);
+    return false;
+  }
 };
