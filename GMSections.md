c1f920b3778bd83fb6eb9e8fa667c337651baf7a
LankeyBoard <56012902+LankeyBoard@users.noreply.github.com>
2026-02-04 22:25:54 -0500

    added things to spend coin on and fixed bug in shop creation with items on sale

--- GMSections.md
+++ GMSections.md
@@ -1,12 +1,12 @@
 # The Basics
 
-SLUG: :GM-BASICS
+slug: GM-BASICS
 
 Fortune's Favor is a game for playing fantasy stories of a band of adventurers going out and changing the world. As the Game Master (GM), your role is to set up the events, respond to players actions and adjudicate the rules for your table.
 
 ## GM Philosophy
 
-SLUG: GM-PHILOSOPHY
+slug: GM-PHILOSOPHY
 
 When running Fortune's Favor, I have found the following tenets helpful to keep in mind.
 
@@ -16,11 +16,11 @@ When running Fortune's Favor, I have found the following tenets helpful to keep 
 
 # During Play
 
-SLUG: GM-DURING-PLAY
+slug: GM-DURING-PLAY
 
 ## Test Difficulty
 
-SLUG: GM-TEST-DIFFICULTY
+slug: GM-TEST-DIFFICULTY
 
 As a GM, you will often have to set Test difficulty on the fly. The difficulty of the Test should reflect how hard the task is as well as how long they have to do it.
 
@@ -35,36 +35,36 @@ If either success or failure would not result in an interesting outcome, that's 
 
 ### Mixed Results
 
-SLUG: GM-MIXED
+slug: GM-MIXED
 
 Often a good mixed result can be a choice of spending Stamina to succeed on the test or letting the test fail. If there's time pressure, then a mixed is also a good time to ratchet up the pressure. Maybe they slip on the jump across the ravine and manage to scramble up the other side, but the beast chasing them is significantly closer, or they manage to swap the cards in a poker game, but weren't slick enough to completely avoid the pit bosses attention.
 
 ### Failures
 
-SLUG: GM-TEST-FAILURE
+slug: GM-TEST-FAILURE
 
 Failing doesn't always have to be narrated as the character not being good enough at the thing to succeed. It's often more satisfying to describe the opponent's skill or the situational challenges that made the character fail rather than their own lacking or incompetence. Failure can also just mean running out of time before something else happens. Like a guard coming around the corner, discovering the party trying to pick a lock, or a troll guarding a bride brushes off an attempt to distract them while the rest of the party sneaks past.
 
 # Planning
 
-SLUG: GM-PLANNING
+slug: GM-PLANNING
 
 ## Running a Session 0
 
-SLUG: GM-SESSION-0
+slug: GM-SESSION-0
 
 Session 0 is your chance to set expectations for the game, give players the pitch for your game if you haven't already and talk about tone. It's also a chance to make characters together and find ways to create bonds between those characters.
 For more information [The Ultimate Session 0 Checklist](https://www.level1geek.com/blog/dnd-session-0) by Level 1 Geek is a good place to start.
 
 ## Making Enemies
 
-SLUG: GM-MAKING-ENEMIES
+slug: GM-MAKING-ENEMIES
 
 Three types of enemies, minions: easy to kill canon fodder, monsters: a reasonable challenge. bosses: a tough challenge. Every boss should provide a unique challenge.
 
 ### Building Encounters
 
-SLUG: GM-BUILDING-ENCOUNTERS
+slug: GM-BUILDING-ENCOUNTERS
 
 An encounter encompasses everything that happens between opportunities for PCs to Catch Your Breath.
 
@@ -73,7 +73,7 @@ These stat's assume 5 encounters per Rest & Recuperation. If you have less encou
 
 #### Base Monster Stats
 
-SLUG: GM-BASE-MONSTER-STATS
+slug: GM-BASE-MONSTER-STATS
 
 These are a good average starting point for creating a monster. When balancing an encounter, whether beforehand or during play, it's better to adjust the
 
@@ -88,9 +88,54 @@ These are a good average starting point for creating a monster. When balancing a
 | 7     | 55     | 15    | +5     | 2d8+5  |
 | 8     | 60     | 15    | +5     | 2d10+5 |
 
+# Treasure & Other Rewards
+
+slug: TREASURE
+
+As characters go on adventures they will inevitably end up with Coin and be looking for things to spend it on. Some players will seek out only mechanically beneficial items, while others will take an interest in cosmetic or socially beneficial items. If your players tend to only look for mechanical benefit. I would recommend increasing some of these costs, and providing more consumable items to give them things to spend coin on without becoming overpowered.
+
+These prices are just baselines and should be adjusted up or down depending on your world and game.
+
+## Consumables
+
+slug: CONSUMABLES
+
+| Name            | Cost | Slots | Description                                                                                                                                                                                                                                                                |
+| --------------- | ---- | ----- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Bandages        | 2    | 1     | When you take a Night's Rest apply bandages and salves, regain an additional 2 Health.                                                                                                                                                                                     |
+| Caltrops        | 1    | 1     | Scatter in a 10ft area circle. Th area either causes the creature to be Slowed, or they take 1d6 Piercing Damage for every 5 ft. they move. Creatures choice.                                                                                                              |
+| Damage Grease   | 3    | 0     | Can be available in any non-physical damage. It takes 1 minute to apply grease to a weapon. When applied the weapon deals an extra 1d4 Damage of the greases type for the next combat the weapon is used in or after 24 hours.                                             |
+| Flash Powder x3 | 2    | 1     | As an Action, toss the powder up to 15 ft. away. Creatures within a 5 ft. radius take an Agility Test (12/7) On a Fail they are Blind until the end of their next turn. On a Mixed, they are Rattled.                                                                      |
+| Healing Potion  | 10   | 1     | When drunk, heals 2d4 Health                                                                                                                                                                                                                                               |
+| Smoke Bomb x3   | 2    | 1     | As an Action, toss the bombs up to 30 ft. away, causing a fast acting smoke to expand from the point of impact.. A 15ft. radius area is instantly obscured, creatures inside are Hidden and Blind. After 4 Rounds (2 Minutes) or a strong wind the area returns to normal. |
+| Supplies        | 1    | 1     | Supplies represent an item or set of mundane items that a standard adventurer is likely to have with them.                                                                                                                                                                 |
+
+## Items
+
+slug: ITEMS
+
+| Name                    | Cost | Slots | Description                                                                                                                                                          |
+| ----------------------- | ---- | ----- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| All Weather Tent        | 2    | 0     | Fits 2 Medium creatures and their gear. Keeps them warm and dry in the harshest environments.                                                                        |
+| Bullseye Lantern        | 2    | 0     | Emits Bright light in a 60 ft. by 5 ft. line. and dim light for an additional 20 ft. forward and 5 ft. to the side.                                                  |
+| Climbing Gear           | 1    | 1     | A set of ropes, spikes and other tools to aid in climbing. Grants +3 on Tests to climb.                                                                              |
+| Horse                   | 3    | 0     | Upkeep 1 Coin per Month.                                                                                                                                             |
+| Tangle Net              | 1    | 1     | Make a Ranged 30ft. (Agility) Attack. On hit target is Pinned until they use an Action to Escape. On a Mixed, they are Entangled until the start of their next turn. |
+| Torch                   | 1    | 0     | Emits Bright light in a 20 ft. sphere. A strong breeze or water puts the flame out.                                                                                  |
+| Waterproof Fire Starter | 1    | 0     | A small box of waterproof matches or alchemical fire starter.                                                                                                        |
+
+## Services
+
+slug: SERVICES
+
+| Name               | Cost | Description                                                                                          |
+| ------------------ | ---- | ---------------------------------------------------------------------------------------------------- |
+| Armor Improvement  | 7    | Increase the benefits of your armor by 1.                                                            |
+| Weapon Improvement | 4    | Increase the Damage or Attack of your weapon by 1. Each weapon can only be improved this way 1 time. |
+
 # Resources
 
-SLUG: RESOURCES
+slug: RESOURCES
 
 Useful resources for running a game.
 
--- generatedgmSections.ts
+++ generatedgmSections.ts
@@ -2,30 +2,14 @@ import { GenericRule } from "../../schema/types.generated";
 const gmSections: GenericRule[] = [
     {
     title: "The Basics",
-    slug: "ERROR",text: [
-    {
-    text: "The Basics",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: :GM-BASICS",
-    type: "RULE"
-    },
+    slug: "GM-BASICS",text: [
     {
     text: "Fortune's Favor is a game for playing fantasy stories of a band of adventurers going out and changing the world. As the Game Master (GM), your role is to set up the events, respond to players actions and adjudicate the rules for your table.",
     type: "RULE"
     },],subRules: [
     {
     title: "GM Philosophy",
-    slug: "ERROR",text: [
-    {
-    text: "GM Philosophy",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-PHILOSOPHY",
-    type: "RULE"
-    },
+    slug: "GM-PHILOSOPHY",text: [
     {
     text: "When running Fortune's Favor, I have found the following tenets helpful to keep in mind.",
     type: "RULE"
@@ -35,26 +19,10 @@ const gmSections: GenericRule[] = [
   }],},]},
     {
     title: "During Play",
-    slug: "ERROR",text: [
-    {
-    text: "During Play",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-DURING-PLAY",
-    type: "RULE"
-    },],subRules: [
+    slug: "GM-DURING-PLAY",subRules: [
     {
     title: "Test Difficulty",
-    slug: "ERROR",text: [
-    {
-    text: "Test Difficulty",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-TEST-DIFFICULTY",
-    type: "RULE"
-    },
+    slug: "GM-TEST-DIFFICULTY",text: [
     {
     text: "As a GM, you will often have to set Test difficulty on the fly. The difficulty of the Test should reflect how hard the task is as well as how long they have to do it.",
     type: "RULE"
@@ -69,86 +37,38 @@ const gmSections: GenericRule[] = [
     },],subRules: [
     {
     title: "Mixed Results",
-    slug: "ERROR",text: [
-    {
-    text: "Mixed Results",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-MIXED",
-    type: "RULE"
-    },
+    slug: "GM-MIXED",text: [
     {
     text: "Often a good mixed result can be a choice of spending Stamina to succeed on the test or letting the test fail. If there's time pressure, then a mixed is also a good time to ratchet up the pressure. Maybe they slip on the jump across the ravine and manage to scramble up the other side, but the beast chasing them is significantly closer, or they manage to swap the cards in a poker game, but weren't slick enough to completely avoid the pit bosses attention.",
     type: "RULE"
     },],},
     {
     title: "Failures",
-    slug: "ERROR",text: [
-    {
-    text: "Failures",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-TEST-FAILURE",
-    type: "RULE"
-    },
+    slug: "GM-TEST-FAILURE",text: [
     {
     text: "Failing doesn't always have to be narrated as the character not being good enough at the thing to succeed. It's often more satisfying to describe the opponent's skill or the situational challenges that made the character fail rather than their own lacking or incompetence. Failure can also just mean running out of time before something else happens. Like a guard coming around the corner, discovering the party trying to pick a lock, or a troll guarding a bride brushes off an attempt to distract them while the rest of the party sneaks past.",
     type: "RULE"
     },],},]},]},
     {
     title: "Planning",
-    slug: "ERROR",text: [
-    {
-    text: "Planning",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-PLANNING",
-    type: "RULE"
-    },],subRules: [
+    slug: "GM-PLANNING",subRules: [
     {
     title: "Running a Session 0",
-    slug: "ERROR",text: [
-    {
-    text: "Running a Session 0",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-SESSION-0",
-    type: "RULE"
-    },
+    slug: "GM-SESSION-0",text: [
     {
     text: "Session 0 is your chance to set expectations for the game, give players the pitch for your game if you haven't already and talk about tone. It's also a chance to make characters together and find ways to create bonds between those characters.\nFor more information [The Ultimate Session 0 Checklist](https://www.level1geek.com/blog/dnd-session-0) by Level 1 Geek is a good place to start.",
     type: "RULE"
     },],},
     {
     title: "Making Enemies",
-    slug: "ERROR",text: [
-    {
-    text: "Making Enemies",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-MAKING-ENEMIES",
-    type: "RULE"
-    },
+    slug: "GM-MAKING-ENEMIES",text: [
     {
     text: "Three types of enemies, minions: easy to kill canon fodder, monsters: a reasonable challenge. bosses: a tough challenge. Every boss should provide a unique challenge.",
     type: "RULE"
     },],subRules: [
     {
     title: "Building Encounters",
-    slug: "ERROR",text: [
-    {
-    text: "Building Encounters",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: GM-BUILDING-ENCOUNTERS",
-    type: "RULE"
-    },
+    slug: "GM-BUILDING-ENCOUNTERS",text: [
     {
     text: "An encounter encompasses everything that happens between opportunities for PCs to Catch Your Breath.",
     type: "RULE"
@@ -158,16 +78,40 @@ const gmSections: GenericRule[] = [
     type: "RULE"
     },],},]},]},
     {
+    title: "Treasure & Other Rewards",
+    slug: "TREASURE",text: [
+    {
+    text: "As characters go on adventures they will inevitably end up with Coin and be looking for things to spend it on. Some players will seek out only mechanically beneficial items, while others will take an interest in cosmetic or socially beneficial items. If your players tend to only look for mechanical benefit. I would recommend increasing some of these costs, and providing more consumable items to give them things to spend coin on without becoming overpowered.",
+    type: "RULE"
+    },
+    {
+    text: "These prices are just baselines and should be adjusted up or down depending on your world and game.",
+    type: "RULE"
+    },],subRules: [
+    {
+    title: "Consumables",
+    slug: "CONSUMABLES",text: [
+    {
+    text: "| Name            | Cost | Slots | Description                                                                                                                                                                                                                                                                |\n| --------------- | ---- | ----- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |\n| Bandages        | 2    | 1     | When you take a Night's Rest apply bandages and salves, regain an additional 2 Health.                                                                                                                                                                                     |\n| Caltrops        | 1    | 1     | Scatter in a 10ft area circle. Th area either causes the creature to be Slowed, or they take 1d6 Piercing Damage for every 5 ft. they move. Creatures choice.                                                                                                              |\n| Damage Grease   | 3    | 0     | Can be available in any non-physical damage. It takes 1 minute to apply grease to a weapon. When applied the weapon deals an extra 1d4 Damage of the greases type for the next combat the weapon is used in or after 24 hours.                                             |\n| Flash Powder x3 | 2    | 1     | As an Action, toss the powder up to 15 ft. away. Creatures within a 5 ft. radius take an Agility Test (12/7) On a Fail they are Blind until the end of their next turn. On a Mixed, they are Rattled.                                                                      |\n| Healing Potion  | 10   | 1     | When drunk, heals 2d4 Health                                                                                                                                                                                                                                               |\n| Smoke Bomb x3   | 2    | 1     | As an Action, toss the bombs up to 30 ft. away, causing a fast acting smoke to expand from the point of impact.. A 15ft. radius area is instantly obscured, creatures inside are Hidden and Blind. After 4 Rounds (2 Minutes) or a strong wind the area returns to normal. |\n| Supplies        | 1    | 1     | Supplies represent an item or set of mundane items that a standard adventurer is likely to have with them.                                                                                                                                                                 |",
+    type: "RULE"
+    },],},
+    {
+    title: "Items",
+    slug: "ITEMS",text: [
+    {
+    text: "| Name                    | Cost | Slots | Description                                                                                                                                                          |\n| ----------------------- | ---- | ----- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------- |\n| All Weather Tent        | 2    | 0     | Fits 2 Medium creatures and their gear. Keeps them warm and dry in the harshest environments.                                                                        |\n| Bullseye Lantern        | 2    | 0     | Emits Bright light in a 60 ft. by 5 ft. line. and dim light for an additional 20 ft. forward and 5 ft. to the side.                                                  |\n| Climbing Gear           | 1    | 1     | A set of ropes, spikes and other tools to aid in climbing. Grants +3 on Tests to climb.                                                                              |\n| Horse                   | 3    | 0     | Upkeep 1 Coin per Month.                                                                                                                                             |\n| Tangle Net              | 1    | 1     | Make a Ranged 30ft. (Agility) Attack. On hit target is Pinned until they use an Action to Escape. On a Mixed, they are Entangled until the start of their next turn. |\n| Torch                   | 1    | 0     | Emits Bright light in a 20 ft. sphere. A strong breeze or water puts the flame out.                                                                                  |\n| Waterproof Fire Starter | 1    | 0     | A small box of waterproof matches or alchemical fire starter.                                                                                                        |",
+    type: "RULE"
+    },],},
+    {
+    title: "Services",
+    slug: "SERVICES",text: [
+    {
+    text: "| Name               | Cost | Description                                                                                          |\n| ------------------ | ---- | ---------------------------------------------------------------------------------------------------- |\n| Armor Improvement  | 7    | Increase the benefits of your armor by 1.                                                            |\n| Weapon Improvement | 4    | Increase the Damage or Attack of your weapon by 1. Each weapon can only be improved this way 1 time. |",
+    type: "RULE"
+    },],},]},
+    {
     title: "Resources",
-    slug: "ERROR",text: [
-    {
-    text: "Resources",
-    type: "RULE"
-    },
-    {
-    text: "SLUG: RESOURCES",
-    type: "RULE"
-    },
+    slug: "RESOURCES",text: [
     {
     text: "Useful resources for running a game.",
     type: "RULE"
